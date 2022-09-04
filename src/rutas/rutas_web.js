d1e4c7c29284a26fb5c2650cfc4167d4a9c8f67f
Grennys <greni00salgado@gmail.com>
2022-09-04 12:21:43 -0500

    endpoints agregar productos agregado

--- src/rutas/rutas_web.js
+++ src/rutas/rutas_web.js
@@ -21,6 +21,33 @@ rutas.get('/productos/:sku', (req, res ) => {
     });     
 });
 
+//Post agregar un producto
+rutas.post('/productos', (req, res ) => {  
+    var date = new Date(); 
+    var fecha = date.toLocaleDateString();   
+    const { sku, nombre,precio,url,marca,descripcion,iva,descuento,inventario } = req.body;    
+    if ( sku && nombre && precio && url && marca && descripcion && iva && descuento && inventario ) {                
+        let NuevoP = {
+            sku, 
+            nombre,
+            precio,
+            url,
+            marca,
+            descripcion,
+            iva,
+            descuento,
+            inventario,
+            fecha
+        };
+        Nprodcuto.push(NuevoP);
+        const nuevoP_json =  JSON.stringify(Nprodcuto);
+        fs.writeFileSync('src/Base_datos/productos.json', nuevoP_json, 'utf-8');
+        res.send('El producto satisfactoriamente');
+    } else {
+        res.status(500).json({error: 'Todos los campos son requeridos.'});
+    }
+});
+
 
 
 module.exports = rutas;
